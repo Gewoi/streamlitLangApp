e1a83971489715ce6fd82f905c27295ca0c2d4f8
Gewoi <gundulea@gmail.com>
2026-07-23 21:53:20 +0200

    new lesson and section

--- auxilliary apps/generator2.py
+++ auxilliary apps/generator2.py
@@ -38,7 +38,7 @@ STEP_TYPES = {
     'match': 'Match Pairs',
 }
 
-SECTIONS = ['Basics', 'Sentence Building', 'Simple Concepts', 'Getting Food', 'Appointments', 'People', 'Jobs']
+SECTIONS = ['Basics', 'Sentence Building', 'Simple Concepts', 'Getting Food', 'Appointments', 'People', 'Jobs', 'Hobbies']
 
 
 def slugify(text: str) -> str:
